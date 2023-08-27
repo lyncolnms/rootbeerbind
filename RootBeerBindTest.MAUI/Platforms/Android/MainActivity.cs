a5e4fbf1dccbf5952dc14962b457be232ee48f75
Lyncoln Mauricio <lyncolnms@gmail.com>
2023-08-27 17:27:08 -0300

    first commit

--- RootBeerBindTest.MAUI/Platforms/Android/MainActivity.cs
+++ RootBeerBindTest.MAUI/Platforms/Android/MainActivity.cs
@@ -1,8 +1,17 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Com.Scottyab.Rootbeer;
 
 namespace RootBeerBindTest.MAUI;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
-public class MainActivity : MauiAppCompatActivity { }
+public class MainActivity : MauiAppCompatActivity {
+    protected override void OnCreate(Bundle savedInstanceState) {
+        RootBeer rootBeer = new RootBeer(this);
+
+
+
+        base.OnCreate(savedInstanceState);
+    }
+}
